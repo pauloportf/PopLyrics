ff7bfd786eeb801a2a07564abc06f12b179dd856
Paulo Cesar <98140547+iampaylin@users.noreply.github.com>
2025-02-05 22:53:24 +0000

    New  with my spotify profile and

--- Assets/JS/index.js
+++ Assets/JS/index.js
@@ -1,13 +1,13 @@
 function toggleMute() {
-    var video = document.getElementById("myVideo");
-    var muteButton = document.getElementById("muteButton");
+  var video = document.getElementById("myVideo");
+  var muteButton = document.getElementById("muteButton");
 
-    if (video.muted) {
-        video.muted = false;
-        muteButton.src = "./Assets/Images/musicon.png"; // Altere para o ícone de desmutado
-        muteButton.style.transition = "0.5s ease";
-    } else {
-        video.muted = true;
-        muteButton.src = "./Assets/Images/musicoff.png"; // Altere para o ícone de mutado
-    }
+  if (video.muted) {
+    video.muted = false;
+    muteButton.src = "./Assets/Images/musicon.png"; // Altere para o ícone de desmutado
+    muteButton.style.transition = "0.5s ease";
+  } else {
+    video.muted = true;
+    muteButton.src = "./Assets/Images/musicoff.png"; // Altere para o ícone de mutado
+  }
 }
