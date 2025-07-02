347ff334899a91c7d3b022042b7749cc328ae230
Paulo Cesar <98140547+pauloportf@users.noreply.github.com>
2025-07-02 16:42:12 +0000

    Pop Lirycs Update

--- Assets/JS/index.js
+++ Assets/JS/index.js
@@ -11,3 +11,52 @@ function toggleMute() {
     muteButton.src = "./Assets/Images/musicoff.png"; // Altere para o ícone de mutado
   }
 }
+
+// variaveis myVideo / coverart / musicinfo / 
+function manchild() {
+  var video = document.getElementById("myVideo")
+  var videoSource = document.getElementById('videoSource');
+  var cover = document.getElementById("coverart")
+  var info = document.getElementById("musicinfo")
+
+  info.innerHTML = "Manchild <br> Sabrina Carpenter"
+
+  if (videoSource.src != "./Assets/Videos/manchild.webm") {
+    videoSource.src = './Assets/Videos/manchild.webm';
+  } else {
+    videoSource.src = './Assets/Videos/manchild.webm';
+  }
+
+  video.load(); // Recarrega o vídeo com a nova fonte
+
+  if (cover.src != "./Assets/Images/songs/manchild.jfif") {
+    cover.src = './Assets/Images/songs/manchild.jfif';
+  } else {
+    cover.src = './Assets/Images/songs/manchild.jfif';
+  }
+}
+
+function clocks() {
+  var video = document.getElementById("myVideo")
+  var videoSource = document.getElementById('videoSource');
+  var cover = document.getElementById("coverart")
+  var info = document.getElementById("musicinfo")
+
+  info.innerHTML = "Clocks <br> Coldplay"
+
+  if (videoSource.src != "./Assets/Videos/Clocks.mkv") {
+    videoSource.src = './Assets/Videos/Clocks.mkv';
+  } else {
+    videoSource.src = './Assets/Videos/Clocks.mkv';
+  }
+
+  video.load(); // Recarrega o vídeo com a nova fonte
+
+  if (cover.src != "./Assets/Images/songs/clocks.jfif") {
+    cover.src = './Assets/Images/songs/clocks.jfif';
+  } else {
+    cover.src = './Assets/Images/songs/clocks.jfif';
+  }
+
+  cover.load();
+}
